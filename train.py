a68f4d6a0f97995337b84f18cdfbddbf943472fe
Yuelin Xin <sc20yx2@leeds.ac.uk>
2023-08-09 11:19:38 -0700

    push training files

---
+++ train.py
@@ -0,0 +1,64 @@
+import torch
+import torchvision
+import torchvision.transforms as transforms
+from model.model import HierarchicalKernelTransformer
+
+
+# Data augmentation and normalization
+transform = transforms.Compose(
+    [transforms.ToTensor(),
+     transforms.Normalize((0.5, 0.5, 0.5), (0.5, 0.5, 0.5))])
+train_set = torchvision.datasets.CIFAR10(root='./data', train=True,
+                                        download=True, transform=transform)
+train_loader = torch.utils.data.DataLoader(train_set, batch_size=4,
+                                          shuffle=True, num_workers=2)
+test_set = torchvision.datasets.CIFAR10(root='./data', train=False,
+                                       download=True, transform=transform)
+test_loader = torch.utils.data.DataLoader(test_set, batch_size=4,
+                                         shuffle=False, num_workers=2)
+classes = ('plane', 'car', 'bird', 'cat',
+           'deer', 'dog', 'frog', 'horse', 'ship', 'truck')
+
+device = torch.device('cuda' if torch.cuda.is_available() else 'cpu')
+print(device)
+
+model = HierarchicalKernelTransformer(in_channels=3, img_dim=32, embed_dim=64, window_size=4, 
+                                      heads=4, depth=4, num_blocks=2).to(device)
+criterion = torch.nn.CrossEntropyLoss()
+optimizer = torch.optim.Adam(model.parameters(), lr=1e-4)
+num_epochs = 100
+
+for epoch in range(num_epochs):
+    model.train()
+    running_loss = 0.0
+    for images, labels in train_loader:
+        images = images.to(device)
+        labels = labels.to(device)
+        
+        optimizer.zero_grad()
+        outputs = model(images)
+        loss = criterion(outputs, labels)
+        loss.backward()
+        optimizer.step()
+        
+        running_loss += loss.item() * images.size(0)
+
+    epoch_loss = running_loss / len(train_set)
+    print(f'Epoch {epoch} loss: {epoch_loss}')
+
+    model.eval()
+    correct, total = 0, 0
+    with torch.no_grad():
+        for images, labels in test_loader:
+            images = images.to(device)
+            labels = labels.to(device)
+            
+            outputs = model(images)
+            _, predicted = torch.max(outputs.data, 1)
+            total += labels.size(0)
+            correct += (predicted == labels).sum().item()
+        
+    accuracy = (correct / total) * 100
+    print(f'Accuracy: {accuracy}')
+
+print('Finished Training')
